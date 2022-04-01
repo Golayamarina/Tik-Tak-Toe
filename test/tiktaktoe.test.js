0c3e7b6544c0ff8fd7542893e9e31467fac3c2c8
Golayamarina <98704212+Golayamarina@users.noreply.github.com>
2022-04-01 12:07:55 +0300

    Create tiktaktoe.test.js

---
+++ test/tiktaktoe.test.js
@@ -0,0 +1,20 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { createModuleResolutionCache } = require("typescript");
+
+describe("TikTakToe", function() {
+    let acc1
+    let acc2
+    let payments
+
+    beforeEach(async function() {
+       [acc1, acc2] = await ethers.getSigners()
+       const TikTakToe = await ethers.getContractFactory("TikTakToe", acc1)
+       tikTakToe = await TikTakToe.deploy()
+       await tikTakToe.deployed()
+       console.log(tikTakToe.address)
+   }) 
+
+   it("shoud be deployed", async function() {
+       expect(tikTakToe.address).to.be.properAddress
+   })
